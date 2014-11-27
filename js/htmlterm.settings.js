860e4eff290bf0489569d12ab267143ce384be9f
Fernando L. Canizo (aka conan) <conan@muriandre.com>
2014-11-27 10:37:16 -0300

    Review all possible HtmlTerm configuration options
    As HtmlTerm author said, I reviewed HtmlTerm source code and set all
    options available. The ones commented were left with default values.

--- js/htmlterm.settings.js
+++ js/htmlterm.settings.js
@@ -1,8 +1,7 @@
-// REENOTE Here's all the settings that can be changed.  Note that this may not always be an up to date list,
-// some settings may be deleted/renamed/added over time, and so this demo may get out of date.  You can open
-// HtmlTerm.js and search for "// Settings", then that should pull up the list of settings.  Reference them
-// by prefixing with HtmlTerm. and dropping the F, for example FBlinkState is set via HtmlTerm.BlinkState
-// To customize any of the settings, just uncomment the line and provide a new value
+
+////////////////////////////////////////////////////////////////////////////////
+// HtmlTerm Settings
+////////////////////////////////////////////////////////////////////////////////
 
 // This setting lets you control the speed of the connection.  It defaults to a high value for quick screen
 // loading, but you can lower it if you want to simulate an old 2400 or something.  (Not tested thoroughly,
@@ -24,12 +23,12 @@
 // HtmlTerm.ConnectionType = "telnet";
 
 // Determines what to send for the enter keypress.  Telnet spec says to send "\r\n", but most BBS software wants "\r";
-// HtmlTerm.Enter = "\r";
+HtmlTerm.Enter = "\r\n";
 
 // Details for the server you want to connect to.  Only port 23 is supported by my proxy, which means only port 23
 // is supported for now.  Later on you'll be able to run your own proxy, and you can use whatever port you want
 // at that time.
-HtmlTerm.ServerName = "fTelnet / HtmlTerm / GameSrv Support Server";
+HtmlTerm.ServerName = "Imperium";
 HtmlTerm.Hostname = "empiredirectory.net";
 HtmlTerm.Port = 3458;
 
@@ -38,24 +37,16 @@ HtmlTerm.Port = 3458;
 HtmlTerm.ProxyHostname = "empiredirectory.net";
 HtmlTerm.ProxyPort = 4321;
 
+// Defined in HtmlTerm.js but without explanation
+// HtmlTerm.ProxyPortSecure = 11235;
+
 // Determines how large the screen will be.  The last row is reserved for a status line
 // HtmlTerm.ScreenColumns = 80;
 // HtmlTerm.ScreenRows = 25;
 
 // This is a Base64 encoded ANSI screen that will be displayed to the user when HtmlTerm first initializes
-// HtmlTerm.SplashScreen = "G1swbRtbMkobWzA7MEgbWzE7NDQ7MzRt2sTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTEG1swOzQ0OzMwbb8bWzBtDQobWzE7NDQ7MzRtsyAgG1szN21XZWxjb21lISAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAbWzA7NDQ7MzBtsxtbMG0NChtbMTs0NDszNG3AG1swOzQ0OzMwbcTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTE2RtbMG0NCg0KG1sxbSAbWzBtIBtbMTs0NDszNG3axMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMQbWzA7NDQ7MzBtvxtbMG0NCiAgG1sxOzQ0OzM0bbMbWzA7MzRt29vb2xtbMzBt29vb29vb29vb29vb29vb29vb29vb2xtbMzRt29vb29vbG1s0NDszMG2zG1swbQ0KICAbWzE7NDQ7MzRtsxtbMDszNG3b29vbG1sxOzMwbdvb29vb29vb29vb29vb29vb29vb29sbWzA7MzBt29sbWzM0bdvb29sbWzQ0OzMwbbMbWzBtDQogIBtbMTs0NDszNG2zG1swOzM0bdvb29sbWzE7MzBt29vb2xtbMG3b29vb29vb29vb29sbWzFt29vb2xtbMzBt29sbWzA7MzBt29sbWzM0bdvb29sbWzQ0OzMwbbMbWzBtDQogIBtbMTs0NDszNG2zG1swOzM0bdvb29sbWzE7MzBt29vb2xtbMG3b29vb29vb29vbG1sxbdvb29sbWzBt29sbWzE7MzBt29sbWzA7MzBt29sbWzM0bdvb29sbWzQ0OzMwbbMbWzBtDQogIBtbMTs0NDszNG2zG1swOzM0bdvb29sbWzE7MzBt29vb2xtbMG3b29vb29vb2xtbMW3b29vbG1swbdvbG1sxbdvbG1szMG3b2xtbMDszMG3b2xtbMzRt29vb2xtbNDQ7MzBtsxtbMG0NCiAgG1sxOzQ0OzM0bbMbWzA7MzRt29vb2xtbMTszMG3b29vbG1swbdvb29vb2xtbMW3b29vbG1swbdvbG1sxbdvb29sbWzMwbdvbG1swOzMwbdvbG1szNG3b29vbG1s0NDszMG2zG1swbQ0KICAbWzE7NDQ7MzRtsxtbMDszNG3b29vbG1sxOzMwbdvb29sbWzBt29vb2xtbMW3b29vbG1swbdvbG1sxbdvb29vb2xtbMzBt29sbWzA7MzBt29sbWzM0bdvb29sbWzQ0OzMwbbMbWzQwOzM3bQ0KICAbWzE7NDQ7MzRtsxtbMDszNG3b29vbG1sxOzMwbdvbG1swOzMwbdvbG1sxbdvb29vb29vb29vb29vb29vb2xtbMDszMG3b2xtbMzRt29vb2xtbNDQ7MzBtsxtbNDA7MzdtDQogIBtbMTs0NDszNG2zG1swOzM0bdvb29sbWzE7MzBt29sbWzBt29vb29vb29vb29vb29vb29vb29sbWzMwbdvbG1szNG3b29vbG1s0NDszMG2zG1s0MDszN20NCiAgG1sxOzQ0OzM0bbMbWzA7MzBt29vb29vb29vb29vb29vb29vb29vb29vb29vb29vbG1szNG3b2xtbNDQ7MzBtsxtbNDA7MzdtDQogIBtbMTs0NDszNG2zG1s0MDszMG3b2xtbMG3b29vb29vb29vb29vb29vb29vb29vb29vb29vbG1szMG3b2xtbNDRtsxtbNDA7MzdtIBtbMzRtIBtbMTs0NzszN23axMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMQbWzMwbb8bWzBtDQogIBtbMTs0NDszNG2zG1swOzMwbdvbG1sxbdvb29vb29vb29vb29vb29sbWzA7MzBt29vb29vb29vb2xtbMW3b2xtbMDszMG3b2xtbNDRtsxtbNDA7MzdtIBtbMzRtIBtbMTs0NzszN22zICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAbWzMwbbMbWzBtDQogIBtbMTs0NDszNG2zG1s0MDszMG3b2xtbMG3b29vb29vb29vb29vb29vb29vb29vb29vb29vbG1szMG3b2xtbNDRtsxtbMG0gG1szNG0gG1sxOzQ3OzM3bbMgICAbWzM0bUh0bWxUZXJtIC0tIFRlbG5ldCBmb3IgdGhlIFdlYiAgICAgG1szMG2zG1swbQ0KG1sxbSAbWzBtIBtbMTs0NDszNG2zG1swOzMwbdvbG1sxbdvb29vb29vb29vb29vb29vb29vb29vb2xtbMDszMG3b29vb29sbWzQ0bbMbWzBtIBtbMzRtIBtbMTs0NzszN22zICAgICAbWzA7NDc7MzRtV2ViIGJhc2VkIEJCUyB0ZXJtaW5hbCBjbGllbnQgICAgG1sxOzMwbbMbWzBtDQogIBtbMTs0NDszNG2zG1swOzM0bdvbG1szMG3b29vb29vb29vb29vb29vb29vb29vb29vb29vbG1szNG3b2xtbNDQ7MzBtsxtbMG0gG1szNG0gG1sxOzQ3OzM3bbMgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIBtbMzBtsxtbMG0NCiAgG1sxOzQ0OzM0bcAbWzA7NDQ7MzBtxMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTZG1swbSAbWzM0bSAbWzE7NDc7MzdtwBtbMzBtxMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTZG1swbQ0KDQobWzExQxtbMTszMm1Db3B5cmlnaHQgKEMpIDIwMDAtMjAxNCBSJk0gU29mdHdhcmUuICBBbGwgUmlnaHRzIFJlc2VydmVkDQobWzA7MzRtxMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExA==";
+// HtmlTerm.SplashScreen -> will be defined in a separate file
 
-// REENOTE There's that HtmlTermContainer id again -- the id you pass
-// here is the id of the element that you want replaced with the
-// HtmlTerm control, so make sure an id with this name exists.
-if (HtmlTerm.Init("HtmlTermContainer")) {
-	// REENOTE HtmlTerm was successfully loaded.
-	// If you want to enable auto-connect, then you could do that here by uncommenting the next line
-	// HtmlTerm.Connect();
+// HtmlTerm.StatusBar = true;
 
-} else {
-	// REENOTE If we get here, it means HtmlTerm failed to load, so we'll pop up an error message.
-	// The HtmlTermContainer element above will also be replaced with an error message, so it's not totally
-	// necessary to pop up an error message here.
-	alert("Sorry, I wasn't able to load HtmlTerm\n\nTry again with an HTML5 capable browser.");
-}
+// HtmlTerm settings ends
