2844ecd831919615434f9e985a15369db8cabbb3
Fernando L. Canizo (aka conan) <conan@muriandre.com>
2014-12-04 08:44:36 -0300

    Replace HtmlTerm with fTelnet
    It's a fork from the same author to fix some issues on Marisa's request.

--- js/htmlterm.settings.js
+++ js/htmlterm.settings.js
@@ -1,55 +1,30 @@
 
+var mg = {}; // Marisa Giancarla namespace to avoid collisions
+
+
 ////////////////////////////////////////////////////////////////////////////////
-// HtmlTerm Settings
+// fTelnet settings
 ////////////////////////////////////////////////////////////////////////////////
-
-// This setting lets you control the speed of the connection.  It defaults to a high value for quick screen
-// loading, but you can lower it if you want to simulate an old 2400 or something.  (Not tested thoroughly,
-// so I'm not sure how accurate it actually will be)
-// HtmlTerm.BitsPerSecond = 115200;
-
-// Determines whether you allow text to blink or not.
-// HtmlTerm.Blink = true;
-
-// Determines which character set to use, and what font size.  I'll need to make a table of all the possible values,
-// for now you'll have to open each font file to see what's available.  For example HtmlTerm.font-437.js has
-// CrtFonts['437x10x19'], which means there's a font with codepage 437 and size 10x19 available.
-// Note you'll have to ensure you add a <script> tag to load the font file containing the font you want.
-// HtmlTerm.CodePage = "437";
-// HtmlTerm.FontWidth = 9;
-// HtmlTerm.FontHeight = 16;
-
-// Determines the connection type.  Will be telnet in most cases, but for example C64 BBSes using tcpser need "raw"
-// HtmlTerm.ConnectionType = "telnet";
-
-// Determines what to send for the enter keypress.  Telnet spec says to send "\r\n", but most BBS software wants "\r";
-HtmlTerm.Enter = "\r\n";
-
-// Details for the server you want to connect to.  Only port 23 is supported by my proxy, which means only port 23
-// is supported for now.  Later on you'll be able to run your own proxy, and you can use whatever port you want
-// at that time.
-HtmlTerm.ServerName = "Imperium";
-HtmlTerm.Hostname = "empiredirectory.net";
-HtmlTerm.Port = 3458;
-
-// HtmlTerm speaks WebSocket, but most BBSes speak telnet, so a proxy server is needed to facilitate communication
-// For now I run the only proxy compatible with my latest HtmlTerm changes, so you'll need to run everything through it
-HtmlTerm.ProxyHostname = "empiredirectory.net";
-HtmlTerm.ProxyPort = 4321;
-
-// Defined in HtmlTerm.js but without explanation
-// HtmlTerm.ProxyPortSecure = 11235;
-
-// Determines how large the screen will be.  The last row is reserved for a status line
-// HtmlTerm.ScreenColumns = 80;
-// HtmlTerm.ScreenRows = 25;
-
-// This is a Base64 encoded ANSI screen that will be displayed to the user when HtmlTerm first initializes
-// HtmlTerm.SplashScreen -> will be defined in a separate file
-
-// HtmlTerm.StatusBar = true;
-
-// HtmlTerm settings ends
+mg.fTelnetSettings = function () {
+	fTelnet.BareLFtoCRLF = true;
+	fTelnet.BitsPerSecond = 57600;
+	fTelnet.Blink = true;
+	fTelnet.ButtonBarVisible = false;
+	fTelnet.ConnectionType = 'telnet'; // only option for the time being, don't touch it
+	fTelnet.Enter = '\r\n';
+	fTelnet.Font = 'CP437';
+	fTelnet.Hostname = 'empiredirectory.net';
+	fTelnet.LocalEcho = true;
+	fTelnet.Port = 3458
+	fTelnet.ProxyHostname = 'empiredirectory.net';
+	fTelnet.ProxyPort = 4321;
+//	fTelnet.ProxyPortSecure = 4321; // I think Marisa doesn't got this one configured
+	fTelnet.ScreenColumns = 80;
+	fTelnet.ScreenRows = 25;
+	fTelnet.SplashScreen = '';
+	fTelnet.StatusBarVisible = false;
+	fTelnet.VirtualKeyboardVisible = false;
+};
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -78,27 +53,26 @@ $('#menuNewDb').on('click', function () {
 $('#menuConnect').on('click', function () {
 	// hide splash screen and show terminal emulator
 	$('#splashScreen').addClass('hidden');
-	$('#HtmlTermContainer').removeClass('hidden');
+	$('#fTelnetContainer').removeClass('hidden');
 
-	if(! HtmlTerm.Init("HtmlTermContainer")) {
-		// If we get here, it means HtmlTerm failed to load, so we'll pop up an error message.
-		// The HtmlTermContainer element above will also be replaced with an error message, so it's not totally
+	if(! fTelnet.Init()) {
+		// If we get here, it means fTelnet failed to load, so we'll pop up an error message.
+		// The fTelnetContainer element above will also be replaced with an error message, so it's not totally
 		// necessary to pop up an error message here.
-		alert("Sorry, I wasn't able to load HtmlTerm\n\nTry again with an HTML5 capable browser.");
+		alert("Sorry, I wasn't able to load fTelnet\n\nTry again with an HTML5 capable browser.");
 		return;
 	}
 
-	Crt.LocalEcho = true; // echo what's typed in terminal
-
-	HtmlTerm.Connect();
+	mg.fTelnetSettings();
+	fTelnet.Connect();
 });
 
 $('#menuDisconnect').on('click', function () {
 	// restore splash screen and hide terminal emulator
-	$('#HtmlTermContainer').addClass('hidden');
+	$('#fTelnetContainer').addClass('hidden');
 	$('#splashScreen').removeClass('hidden');
 
-	HtmlTerm.Disconnect();
+	fTelnet.Disconnect();
 });
 
 
